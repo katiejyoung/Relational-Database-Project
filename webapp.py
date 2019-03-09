98e29759369eef17f42055bc48dcfc10a10c3055
Eric Newton <newtoner@oregonstate.edu>
2019-03-09 01:54:08 -0800

    Director UPDATE implemented

--- webapp.py
+++ webapp.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from flask import request
 from db_connector.db_connector import connect_to_database, execute_query
+import logging
+import string
 #create the web application
 webapp = Flask(__name__)
+webapp.secret_key = 'vxBQYZpvBn'
 
 @webapp.route('/')
 def home():
@@ -48,22 +51,75 @@ def actors():
 #   print(result)
     return render_template('actors.html')
 
-@webapp.route('/directors')
+@webapp.route('/directors', methods=['POST','GET'])
 def directors():
-#    db_connection = connect_to_database()
-#    query = "SELECT id, title, language, year, runtime from film;"
-#    result = execute_query(db_connection, query).fetchall();
-#    print(result)
-    return render_template('directors.html')
+    valid_update_query = False
+    
+    if request.method == 'POST':
+        director_selected = request.form.get('director_select');
+        first_name = request.form['fname']
+        last_name = request.form['lname']
+        year_born = request.form['year_born']
+        year_died = request.form['year_died']
+        valid_update_query = True
+    else:
+        director_selected = 1
+        valid_update_query = False
+
+    # If the user has potentially entered UPDATE information, prepare the UPDATE query
+    if valid_update_query:
+        update_string = ''
+        num_args = 0
+        
+        if first_name != '':
+            update_string += "first_name = \'" + first_name + '\', '
+            num_args += 1
+        if last_name != '':
+            update_string += "last_name = \'" + last_name + '\', '
+            num_args += 1
+        if year_born != '':
+            update_string += "year_born = \'" + year_born + '\', '
+            num_args += 1
+        if year_died != '':
+            update_string += "year_died = \'" + year_died + '\' '
+            num_args += 1
+
+        # If no arguments, this is not an UPDATE query
+        if num_args == 0:
+            valid_update_query = False
+        elif num_args >= 1: # If one or more arguments, strip trailing comma
+            update_string = update_string.rstrip(', ') + ' '
+    
+    # If this has proven to be a UPDATE query, perform it
+    db_connection = connect_to_database()
+    
+    if valid_update_query:
+        update_query = "UPDATE director SET " + update_string + "WHERE id = %s" % (director_selected)
+        execute_query(db_connection, update_query)
+        flash("Entry updated!")
+
+        # If the user has not entered a fname, lname, and year_born, this is an invalid query
+        #if fname = '' || lname = '' || year_born = '':
+        #    valid_query = False
+
+    # Populate Film Dropdown
+    query = "SELECT id, last_name FROM director;"
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    
+    # Populate Films with Selected Director Table
+    query2 = "SELECT title FROM film f INNER JOIN film_direction d ON f.id = d.film_id AND d.director_id = %s" % (director_selected)
+    result2 = execute_query(db_connection, query2).fetchall();
+    return render_template('directors.html', directors=result, director_id=director_selected, rows=result2);
 
 #display update form and process any updates, using the same function
-@webapp.route('/update_people/<id>', methods=['POST','GET'])
-def update_people(id):
+@webapp.route('/update_director/', methods=['POST','GET'])
+def update_director(id):
     db_connection = connect_to_database()
     if request.method == 'GET':
 
-        people_query = 'SELECT character_id, fname, lname, homeworld, age from bsg_people WHERE character_id = %s' % (id) 
-        people_result = execute_query(db_connection, people_query).fetchone()
+        director_query = 'SELECT first_name, last_name, year_born, year_died from director WHERE id = %s' % (id) 
+        director_result = execute_query(db_connection, people_query).fetchone()
 
         if people_result == None:
             return "No such person found!"
