b84e03ee0153d30e67fb0b0aaa5634320453b9e5
Eric Newton <newtoner@oregonstate.edu>
2019-03-09 17:41:41 -0800

    Beginning of film DELETE

--- webapp.py
+++ webapp.py
@@ -25,8 +25,14 @@ def films():
         insert_query = 'INSERT INTO film (title, language, year, runtime) VALUES (%s,%s,%s,%s)'
         data = (title, language, year, runtime)
         execute_query(db_connection, insert_query, data)
-       #return ('Film added!');
-        
+        flash('Film added!');
+    #elif request.method == 'GET':
+       # Film being deleted
+    #    id_to_delete = request.form['DELETE']
+    #    if id_to_delete != '':
+    #        webapp.logger.error(id_to_delete)
+
+    # Display films in DB
     query = "SELECT id, title, language, year, runtime from film;"
     result = execute_query(db_connection, query).fetchall();
     print(result)
@@ -140,55 +146,3 @@ def directors():
     query2 = "SELECT id, title, language, year, runtime FROM film f INNER JOIN film_direction d ON f.id = d.film_id AND d.director_id = %s" % (director_selected)
     result2 = execute_query(db_connection, query2).fetchall();
     return render_template('directors.html', directors=result, director_id=director_selected, rows=result2);
-
-#display update form and process any updates, using the same function
-#@webapp.route('/update_director/', methods=['POST','GET'])
-#def update_director(id):
-#    db_connection = connect_to_database()
-#    if request.method == 'GET':
-
-#        director_query = 'SELECT first_name, last_name, year_born, year_died from director WHERE id = %s' % (id) 
-#        director_result = execute_query(db_connection, people_query).fetchone()
-
-#        if people_result == None:
-#            return "No such person found!"
-
-#        planets_query = 'SELECT planet_id, name from bsg_planets'
-#        planets_results = execute_query(db_connection, planets_query).fetchall();
-
-#        return render_template('people_update.html', planets = planets_results, person = people_result)
-#    elif request.method == 'POST':
-#        print("Update people!");
-#        character_id = request.form['character_id'] 
-#        fname = request.form['fname']
-#        lname = request.form['lname']
-#        age = request.form['age']
-#        homeworld = request.form['homeworld']
-
-#        print(request.form);
-
-#        query = "UPDATE bsg_people SET fname = %s, lname = %s, age = %s, homeworld = %s WHERE character_id = %s"
-#        data = (fname, lname, age, homeworld, character_id)
-#        result = execute_query(db_connection, query, data)
-#        return (str(result.rowcount) + " row(s) updated");
-
-#@webapp.route('/add_new_people', methods=['POST','GET'])
-#def add_new_people():
-#    db_connection = connect_to_database()
-#    if request.method == 'GET':
-#        query = 'SELECT id, title from film'
-#        result = execute_query(db_connection, query).fetchall();
-#        print(result)
-
-#        return render_template('people_add_new.html', planets = result)
-#    elif request.method == 'POST':
-#        print("Add new people!");
-#        fname = request.form['fname']
-#        lname = request.form['lname']
-#        age = request.form['age']
-#        homeworld = request.form['homeworld']
-
-#        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
-#        data = (fname, lname, age, homeworld)
-#        execute_query(db_connection, query, data)
-#        return ('Person added!');
